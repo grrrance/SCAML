d23ddb996b858e2e83d5e75b73ca1dac430527b3
matvey <kalashnikov.matvey.m@gmail.com>
2024-02-21 01:34:51 +0300

    fix

--- SCAML/lib/asm.ml
+++ SCAML/lib/asm.ml
@@ -23,13 +23,13 @@ let stack_offset = ref 0
 let asm_code = Buffer.create 128
 
 let reg_from_arg = function
-  | 0 -> "rdi"
-  | 1 -> "rsi"
-  | 2 -> "rdx"
-  | 3 -> "rcx"
-  | 4 -> "r8"
-  | 5 -> "r9"
-  | _ -> failwith "Too many arguments"
+  | 0 -> ok "rdi"
+  | 1 -> ok "rsi"
+  | 2 -> ok "rdx"
+  | 3 -> ok "rcx"
+  | 4 -> ok "r8"
+  | 5 -> ok "r9"
+  | _ -> error "Too many arguments"
 ;;
 
 let buid_store asm_val =
@@ -49,10 +49,14 @@ let build_call name args =
       | Reg value -> value
       | Imm value -> value)
   in
-  Base.List.iteri args ~f:(fun i arg ->
-    Buffer.add_string asm_code (Printf.sprintf "  mov %s, %s\n" (reg_from_arg i) arg));
+  let* _ = Base.List.foldi args ~init:(ok ()) ~f:(fun i acc arg ->
+    let* _ = acc in
+    let* reg = reg_from_arg i in 
+    Buffer.add_string asm_code (Printf.sprintf "  mov %s, %s\n" reg arg);
+    ok ())
+  in
   Buffer.add_string asm_code (Printf.sprintf "  call %s\n" name);
-  buid_store (reg "rax")
+  ok (buid_store (reg "rax"))
 ;;
 
 let codegen_imm env = function
@@ -66,7 +70,7 @@ let codegen_imm env = function
         | Some arg_num ->
           (* get function address *)
           Buffer.add_string asm_code (Printf.sprintf "  mov rax, %s\n" id);
-          Ok (build_call "addNewPAppliClosure" [ reg "rax"; imm (Int.to_string arg_num) ])
+          build_call "addNewPAppliClosure" [ reg "rax"; imm (Int.to_string arg_num) ]
         | None -> Error "Global function not found"))
   | ImmUnit -> Ok (imm "0")
 ;;
@@ -167,7 +171,7 @@ let rec codegen_cexpr env = function
   | CApp (func, argument) ->
     let* calee = codegen_imm env func in
     let* arg = codegen_imm env argument in
-    ok (build_call "applyPAppli" [ calee; arg ])
+    build_call "applyPAppli" [ calee; arg ]
   | CIf (cond, then_, else_) ->
     let* cond = codegen_imm env cond in
     let else_label = Printf.sprintf "else_%d" !if_counter in
@@ -189,9 +193,9 @@ let rec codegen_cexpr env = function
     let* else_ = codegen_aexpr env else_ in
     let else_mem =
       match else_ with
-      | Mem value -> (
+      | Mem value ->
         Buffer.add_string asm_code (Printf.sprintf "  mov rax, %s\n" value);
-        "rax")
+        "rax"
       | Reg value -> value
       | Imm value -> value
     in
@@ -234,19 +238,18 @@ let codegen_bexpr = function
       in
       check [] args
     in
-    let env =
+    let* env =
       Base.List.foldi
         ~f:(fun i env id ->
           if id <> "0_unused"
           then
             if i < 6
             then (
+              let* env = env in
+              let* reg = reg_from_arg i in
               Buffer.add_string
                 asm_code
-                (Printf.sprintf
-                   "  mov qword [rbp %d], %s\n"
-                   !stack_offset
-                   (reg_from_arg i));
+                (Printf.sprintf "  mov qword [rbp %d], %s\n" !stack_offset reg);
               let env =
                 Base.Map.Poly.set
                   env
@@ -254,14 +257,16 @@ let codegen_bexpr = function
                   ~data:(Printf.sprintf "qword [rbp %d]" !stack_offset)
               in
               stack_offset := !stack_offset - 8;
-              env)
+              ok env)
             else
-              Base.Map.Poly.set
-                env
-                ~key:id
-                ~data:(Printf.sprintf "qword [rbp + %d]" ((8 * (i - 5)) + 8))
+              let* env = env in
+              ok
+                (Base.Map.Poly.set
+                   env
+                   ~key:id
+                   ~data:(Printf.sprintf "qword [rbp + %d]" ((8 * (i - 5)) + 8)))
           else env)
-        ~init:env
+        ~init:(ok env)
         names
     in
     Hashtbl.add global_env id (List.length args);
