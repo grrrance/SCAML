d4797d9a656924d74446389c7c698b4432eb9f94
matvey <kalashnikov.matvey.m@gmail.com>
2024-02-29 14:45:19 +0300

    fix naming

--- SCAML/lib/resultCounter.mli
+++ SCAML/lib/resultCounter.mli
@@ -11,11 +11,11 @@ end
 
 module type RESULTSTATE = sig
   type state
-  type errorMsg
+  type e_msg
 
   type 'a res =
     | ROk of 'a
-    | RErr of errorMsg
+    | RErr of e_msg
 
   include MONAD
 
@@ -24,18 +24,18 @@ module type RESULTSTATE = sig
   end
 
   val ok : 'a -> 'a t
-  val error : errorMsg -> 'a t
+  val error : e_msg -> 'a t
   val get : state t
   val put : state -> unit t
   val runResState : 'a t -> init:state -> state * 'a res
 end
 
 module ResState (S : sig
-    type eMsg
+    type e_msg
     type st
-  end) : RESULTSTATE with type state = S.st and type errorMsg = S.eMsg
+  end) : RESULTSTATE with type state = S.st and type e_msg = S.e_msg
 
-module IResState : RESULTSTATE with type state = int * int and type errorMsg = string
+module IResState : RESULTSTATE with type state = int * int and type e_msg = string
 
 val fresh_if : (int * int) IResState.t
 val decr_offset : (int * int) IResState.t
